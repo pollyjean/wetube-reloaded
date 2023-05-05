32add735e90d1a27d39b90ffafeeba74696d402b
pollyjean <pollyjean@naver.com>
2023-05-06 00:09:05 +0900

    에러 처리 부분 추가, 스키마 보완

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,25 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  try {
-    const videos = await Video.find({});
-    return res.render("home", { pageTitle: "Home", videos });
-  } catch {
-    return res.render("Server Error");
-  }
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Video Upload" });
+};
+export const postUpload = async (req, res) => {
+  const { title, desc, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      desc,
+      hashtags: hashtags.split(",").map((word) => (word.trim().substr(0, 1) === "#" ? word : `#${word}`)),
+    });
+  } catch (error) {
+    return res.render("upload", { pageTitle: "Video Upload", errorMessage: error._message });
+  }
+  return res.redirect("/");
+};
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching: ${video.title}` });
@@ -21,11 +32,4 @@ export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
-};
-export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: "Video Upload" });
-};
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
 };
