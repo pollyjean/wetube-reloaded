5d8bf27b0cdc13d98e8198bd2a7c39ba39074da9
pollyjean <pollyjean@naver.com>
2023-05-07 15:40:08 +0900

    에디트 진행 중

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,23 +13,39 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       desc,
-      hashtags: hashtags.split(",").map((word) => (word.trim().substr(0, 1) === "#" ? word : `#${word}`)),
+      hashtags: hashtags.split(",").map((word) => (word.substr(0, 1) === "#" ? word : `#${word}`)),
     });
   } catch (error) {
     return res.render("upload", { pageTitle: "Video Upload", errorMessage: error._message });
   }
   return res.redirect("/");
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching: ${video.title}` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing: ${video.title}` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, desc, hashtags } = req.body;
+  const video = await Video.findByIdAndUpdate(id, {
+    title,
+    desc,
+    hashtags: hashtags.split(",").map((word) => (word.substr(0, 1) === "#" ? word : `#${word}`)),
+  });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.redirect(`/videos/${id}`);
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  desc: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  desc: { type: String, required: true, trim: true, maxLength: 255, minLength: 20 },
   createdAt: { type: Date, default: Date.now, required: true },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
