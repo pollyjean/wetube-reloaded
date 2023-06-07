afad6e9bd8f88c72215b967ffda12d857c17a274
pollyjean <pollyjean@naver.com>
2023-06-08 04:00:14 +0900

    배포 후 확인, 버그 수정
    - package-lock.json 변경 사항 반영
    - init에 통신 포트 변수 적용, 로그 문구 변경
    - 프로필 자기 영상에 썸네일 안나오는 오류 수정
    - 프로필 수정 버튼 나오지 않는 오류 수정
    - 프로필 하단 비디오 비로그인 안보이게

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./app";
 
-const PORT = 4000;
-const handleListen = () => console.log(`server listening.. on a next level http://localhost:${PORT}`);
+const PORT = process.env.PORT || 4000;
+const handleListen = () => console.log(`server listening... Port : ${PORT}`);
 
 app.listen(PORT, handleListen);
